6d9576fe0774f8441e7235b5863cff9b97a6016d
Harry <harrycheongjunkai@gmail.com>
2023-03-15 22:03:03 +0800

    nav, files page

--- defender/src/App.vue
+++ defender/src/App.vue
@@ -1,26 +1,64 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-app>
+    <v-navigation-drawer
+      v-model="drawer"
+      :rail="rail"
+      permanent
+      @click="rail = !rail"
+    >
+      <v-list-item
+        prepend-avatar="https://images.unsplash.com/photo-1594003715326-f6030861574e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2NlbmVyeSUyMHdhbGxwYXBlcnxlbnwwfHwwfHw%3D&w=1000&q=80"
+        title="Harry"
+        nav
+      >
+        <template v-slot:append>
+          <v-btn
+            variant="text"
+            icon="mdi-chevron-left"
+            @click.stop="rail = !rail"
+          ></v-btn>
+        </template>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list density="compact">
+        <v-list-item
+          v-for="item in items"
+          :prepend-icon="item.icon"
+          :title="item.title"
+          :value="item.value"
+        ></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar>
+      <v-toolbar-title class="center-align" dark>SafeDrive</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Vue from "vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  data() {
+    return {
+      drawer: true,
+      rail: true,
+      items: [
+        { title: "Home", icon: "mdi-home-city", value: "home" },
+        { title: "Drive", icon: "mdi-account", value: "drive" },
+      ],
+    };
+  },
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  methods: {},
+};
+</script>
--- defender/src/main.js
+++ defender/src/main.js
@@ -1,4 +1,38 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
 
-createApp(App).mount('#app')
+// Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import "@mdi/font/css/materialdesignicons.css";
+
+import App from "./App.vue";
+import Main from "./views/Main";
+import Drive from "./views/Drive";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: "/", component: Main },
+    { path: "/drive", component: Drive },
+  ],
+
+  scrollBehaviour(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    }
+    return { left: 0, top: 0 };
+  },
+});
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+const app = createApp(App);
+
+app.use(router);
+app.use(vuetify);
+app.mount("#app");
